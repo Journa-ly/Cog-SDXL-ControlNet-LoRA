7909b38197215c5c90ca6f8da085e4b860500766
Nick Harvey <641094+Nick-Harvey@users.noreply.github.com>
2024-05-08 13:18:20 -0400

    adding lora download step to build process

--- script/download_weights.py
+++ script/download_weights.py
@@ -1,14 +1,66 @@
 import torch
+import os
+from dotenv import load_dotenv
+from azure.storage.blob import BlobServiceClient
 from diffusers import AutoencoderKL, DiffusionPipeline, ControlNetModel
 from diffusers.pipelines.stable_diffusion.safety_checker import (
     StableDiffusionSafetyChecker,
 )
 
+# Load environment variables from the .env file
+load_dotenv()
 
 CONTROL_CACHE = "control-cache"
 SDXL_MODEL_CACHE = "./sdxl-cache"
 REFINER_MODEL_CACHE = "./refiner-cache"
 SAFETY_CACHE = "./safety-cache"
+
+
+# # Directories and Constants (loaded from environment variables)
+# AZURE_ACCOUNT_URL = os.getenv("AZURE_ACCOUNT_URL")
+# JOURNA_CONTAINER_NAME = os.getenv("JOURNA_CONTAINER_NAME")
+# JOURNA_BLOB_NAME = os.getenv("JOURNA_BLOB_NAME")
+# JOURNA_MODEL_LOCAL_PATH = os.getenv("JOURNA_MODEL_LOCAL_PATH")
+# SAS_TOKEN = os.getenv("SAS_TOKEN")
+
+# # Journa Azure Storage URL and Blob Information
+# JOURNA_CONTAINER_NAME = ""
+# JOURNA_BLOB_NAME = ""
+# AZURE_ACCOUNT_URL = ""
+# SAS_token = ""
+
+# # Function to get BlobServiceClient
+# def get_blob_service_client_sas(sas_token: str) -> BlobServiceClient:
+#     return BlobServiceClient(account_url=AZURE_ACCOUNT_URL, credential=sas_token)
+
+# # Function to download blob to a file
+# def download_blob_to_file(blob_service_client: BlobServiceClient, container_name: str, blob_name: str, download_path: str):
+#     # Create blob client
+#     blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+    
+#     # Create local directory if not exists
+#     os.makedirs(os.path.dirname(download_path), exist_ok=True)
+
+#     # Download the blob to the specified file
+#     with open(download_path, "wb") as file:
+#         download_stream = blob_client.download_blob()
+#         file.write(download_stream.readall())
+
+# # SAS token for authentication (Replace this with your actual token)
+# sas_token = "<YOUR_SAS_TOKEN>"
+
+# # Get the blob service client
+# blob_service_client = get_blob_service_client_sas(sas_token)
+
+# # Download the Journa model
+# download_blob_to_file(
+#     blob_service_client=blob_service_client,
+#     container_name=JOURNA_CONTAINER_NAME,
+#     blob_name=JOURNA_BLOB_NAME,
+#     download_path=JOURNA_MODEL_LOCAL_PATH
+# )
+
+
 
 better_vae = AutoencoderKL.from_pretrained(
     "madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16
